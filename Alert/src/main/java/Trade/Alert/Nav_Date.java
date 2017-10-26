cb7e2dc37b98af27e22c5b570781331bc95c19f5
MIG-CAPITAL <61602596@qq.com>
2017-10-26 23:41:44 +0800

    maven project
    1st setup

---
+++ Alert/src/main/java/Trade/Alert/Nav_Date.java
@@ -0,0 +1,11 @@
+package Trade.Alert;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+public class Nav_Date {
+	Logger logger = new Logger("hello");
+Date date = new Date();
+logger.info (date);
+
+}
